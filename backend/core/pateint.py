6bb086b1688bd6556c7f669731d7b2f7a2089898
M-Nhut <nnhut161105@gmail.com>
2026-06-30 19:45:34 +0700

    delete comment

--- backend/core/pateint.py
+++ backend/core/pateint.py
@@ -3,11 +3,11 @@ import time
 
 class Patient:
     def __init__(self, duration: int):
-        self.id = str(uuid.uuid4())[:8] # Định nghĩa ID rút gọn [cite: 2]
-        self.arrival_time = int(time.time()) # Đồng bộ mốc thời gian thực theo giây hệ thống [cite: 5]
-        self.duration = duration # Thời gian khám [cite: 2]
-        self.start_time = None # Thời gian bắt đầu khám [cite: 2]
-        self.end_time = None # Thời gian kết thúc khám [cite: 2]
+        self.id = str(uuid.uuid4())[:8] 
+        self.arrival_time = int(time.time()) 
+        self.duration = duration 
+        self.start_time = None 
+        self.end_time = None 
 
     def to_dict(self):
         return {
